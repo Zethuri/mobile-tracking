dabe42f04240e387b3f7546d1f1adafe2a9fbcaf
Zethuri <anukamvalentine09@gmail.com>
2024-12-24 19:32:14 +0100

    register page added

--- index.php
+++ index.php
@@ -11,13 +11,14 @@
         <div class="landing-section">
             <img src="./dist/images/tracking-logo2.webp" alt="tracking-logo" class="logo-img">
             <h1>Zethuri Tracker</h1>
+            <h2>Welcome</h2>
         </div>
     </section>
 
       <script>
     setTimeout(() => {
       window.location.href = 'register.php'; // Change this to your register page
-    }, 3000); // 3 seconds delay
+    }, 4000); // 3 seconds delay
   </script>
 
 </body>
---
+++ register.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="registerSection">
+            <div class="formHolder">
+                <img src="./dist/images/tracking-logo2.webp" alt="tracking logo" class="reg-img">
+                <h2>Create an account</h2>
+                <form action="submit_form.php" method="POST">
+                    <div class="form-group">
+                        <label for="full-name">Full name</label>
+                        <input type="text" id="full-name" name="full-name" placeholder="Amélie Laurent" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="amelielaurent7622@gmail.com" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" placeholder="********" required>
+                        <span class="toggle-password">👁</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Confirm Password</label>
+                        <input type="password" id="password" name="password" placeholder="********" required>
+                        <span class="toggle-password">👁</span>
+                    </div>
+                    <button type="submit" class="submit-btn">Submit</button>
+                    <p class="">Already have an account? <a href="login.php">Login</a></p>
+                </form>
+            </div>
+    </section>
+    <script>
+        document.querySelector('.toggle-password').addEventListener('click', function () {
+        const passwordField = document.getElementById('password');
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            this.textContent = '🙈';
+        } else {
+            passwordField.type = 'password';
+            this.textContent = '👁';
+        }
+});
+
+    </script>
+</body>
+</html>
